54820f4341b5fbae901e03c4dbcb5853f233db35
Artyom Afonin <0x000552@gmail.com>
2023-01-13 17:18:43 +0800

    UI ready

--- src/app/awesome-icon-generator/awesome-icon-generator.component.ts
+++ src/app/awesome-icon-generator/awesome-icon-generator.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import * as icons from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-awesome-icon-generator',
   templateUrl: './awesome-icon-generator.component.html',
   styleUrls: ['./awesome-icon-generator.component.css']
 })
-export class AwesomeIconGeneratorComponent {
 
+export class AwesomeIconGeneratorComponent {
+  generatedIcon = icons.fa0;
+  isButtonDisabled = false;
+  isIconHidden = true;
+
+  generateIcon() {
+   
+  }
 }
